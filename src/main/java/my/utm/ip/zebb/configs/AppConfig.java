048930c520e08cc43e6a127d32dc7761ea3152ea
Loy <tanzenchai0923@gmail.com>
2024-01-23 00:25:43 +0800

    merging

--- src/main/java/my/utm/ip/zebb/configs/AppConfig.java
+++ src/main/java/my/utm/ip/zebb/configs/AppConfig.java
@@ -3,10 +3,14 @@ package my.utm.ip.zebb.configs;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import my.utm.ip.zebb.models.user.repository.UserRepository;
+import my.utm.ip.zebb.models.user.repository.UserRepository_JDBC;
+import my.utm.ip.zebb.services.user.UserService_JDBC;
 import my.utm.ip.zebb.models.product.ProductRepository;
 import my.utm.ip.zebb.models.product.ProductRepository_JDBC;
 import my.utm.ip.zebb.services.product.ProductService;
 import my.utm.ip.zebb.services.product.ProductService_Database;
+import my.utm.ip.zebb.services.user.UserService;
 
 @Configuration
 public class AppConfig {
@@ -23,5 +27,16 @@ public class AppConfig {
     @Bean
     ProductRepository productRepository() {
         return new ProductRepository_JDBC();
+    }
+
+    @Bean
+    UserService userService() {
+        // return new ProductService_Memory();
+        return new UserService_JDBC();
+    }
+
+    @Bean
+    UserRepository userRepository() {
+        return new UserRepository_JDBC();
     }
 }
