28c7f392c573fa47c829530fcddbb85e14694926
lazyyam <yyzhan020623@gmail.com>
2024-01-23 23:43:12 +0800

    renew database config

--- src/main/java/my/utm/ip/zebb/configs/DatabaseConfig.java
+++ src/main/java/my/utm/ip/zebb/configs/DatabaseConfig.java
@@ -29,9 +29,25 @@ public class DatabaseConfig {
 
 
 
-
     
 
+    //Loy's database
+    // private DriverManagerDataSource getDataSource(){
+    //     DriverManagerDataSource dataSource = new DriverManagerDataSource();
+    //     dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+    //     dataSource.setUrl("jdbc:mysql://localhost:3306/mydb");
+    //     dataSource.setUsername("root");
+    //     dataSource.setPassword("aeiou12345");
+
+    //     return dataSource;
+    // }
+
+    // @Bean
+    // JdbcTemplate jdbcTemplate(){
+    //      return new JdbcTemplate(getDataSource());
+    // }
+    
+    //yam's database
     // private DriverManagerDataSource getDataSource(){
     //     DriverManagerDataSource dataSource = new DriverManagerDataSource();
     //     dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
