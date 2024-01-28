9ca4ca7296c9c32930c823057bb0c28a8ae600fb
Loy <tanzenchai0923@gmail.com>
2024-01-29 00:01:12 +0800

    merging

--- src/main/java/my/utm/ip/zebb/controllers/UserController.java
+++ src/main/java/my/utm/ip/zebb/controllers/UserController.java
@@ -4,6 +4,8 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -19,30 +21,38 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping("/login")
-    public String login(@RequestParam("username") String username,
-                        @RequestParam("password") String password,
-                        HttpSession session){
+    @RequestMapping("/loginvalidation")
+    public String loginvalidation(@RequestParam(value = "username",required = false) String username,
+                        @RequestParam(value = "password",required = false) String password,
+                        @RequestParam(value = "error",required = false) String olderror,
+                        HttpSession session,Model model){
         User currentUser = userService.login(username);
-
-        if(currentUser !=null){
+        String error = "Your username or password is incorrect";
+        if(currentUser.getId()!=0){
 
         if(currentUser.getPassword().equals(password)){
 
             currentUser.setAuthenticated(true);
-
+            session.setAttribute("user", currentUser);
         }else{
-
+            if (olderror == null) {
+                model.addAttribute("error", error);
+            }
             currentUser.setAuthenticated(false);
 
-        }
-
-        session.setAttribute("user", currentUser);
+        }       
 
         }
+        else{
+            if (olderror == null) {
+            model.addAttribute("error", error);
+            }
+        }
+
         return "main/index";
 
     }
+
 
     @RequestMapping("/register")
     public String register(){
