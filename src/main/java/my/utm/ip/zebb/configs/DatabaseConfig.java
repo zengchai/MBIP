108e852ee8f1bd291914dd2ab461a1379b2b7e7d
Zechih <02zechih@gmail.com>
2024-01-30 18:37:26 +0800

    git ze push

--- src/main/java/my/utm/ip/zebb/configs/DatabaseConfig.java
+++ src/main/java/my/utm/ip/zebb/configs/DatabaseConfig.java
@@ -1,6 +1,5 @@
 package my.utm.ip.zebb.configs;
 
-
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
@@ -10,51 +9,51 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class DatabaseConfig {
 
-    private DriverManagerDataSource getDataSource(){
+    private DriverManagerDataSource getDataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/zebb_yam");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/mbip_db");
         dataSource.setUsername("root");
-        dataSource.setPassword("admin");
+        dataSource.setPassword("handsome5");
 
         return dataSource;
     }
 
     @Bean
-    JdbcTemplate jdbcTemplate(){
-         return new JdbcTemplate(getDataSource());
+    JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(getDataSource());
     }
 
-
-    //Loy's database
+    // Loy's database
     // private DriverManagerDataSource getDataSource(){
-    //     DriverManagerDataSource dataSource = new DriverManagerDataSource();
-    //     dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-    //     dataSource.setUrl("jdbc:mysql://localhost:3306/mydb");
-    //     dataSource.setUsername("root");
-    //     dataSource.setPassword("aeiou12345");
+    // DriverManagerDataSource dataSource = new DriverManagerDataSource();
+    // dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+    // dataSource.setUrl("jdbc:mysql://localhost:3306/mydb");
+    // dataSource.setUsername("root");
+    // dataSource.setPassword("aeiou12345");
 
-    //     return dataSource;
+    // return dataSource;
     // }
 
     // @Bean
     // JdbcTemplate jdbcTemplate(){
-    //      return new JdbcTemplate(getDataSource());
+    // return new JdbcTemplate(getDataSource());
     // }
-    
-    //yam's database
-    // private DriverManagerDataSource getDataSource(){
-    //     DriverManagerDataSource dataSource = new DriverManagerDataSource();
-    //     dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-    //     dataSource.setUrl("jdbc:mysql://localhost:3306/zebb_yam");
-    //     dataSource.setUsername("root");
-    //     dataSource.setPassword("admin");
 
-    //     return dataSource;
+    // yam's database
+    // private DriverManagerDataSource getDataSource(){
+    // DriverManagerDataSource dataSource = new DriverManagerDataSource();
+    // dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+    // dataSource.setUrl("jdbc:mysql://localhost:3306/zebb_yam");
+    // dataSource.setUsername("root");
+    // dataSource.setPassword("admin");
+
+    // return dataSource;
     // }
 
     // @Bean
     // JdbcTemplate jdbcTemplate(){
-    //      return new JdbcTemplate(getDataSource());
+    // return new JdbcTemplate(getDataSource());
     // }
+
 }
