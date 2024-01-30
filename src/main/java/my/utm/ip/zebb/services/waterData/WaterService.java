53733991b857d3a9b90bac59c937c781b3a74b6e
Loy <tanzenchai0923@gmail.com>
2024-01-30 18:50:03 +0800

    Adding gitignore

---
+++ src/main/java/my/utm/ip/zebb/services/waterData/WaterService.java
@@ -0,0 +1,14 @@
+package my.utm.ip.zebb.services.waterData;
+
+import java.util.*;
+
+import my.utm.ip.zebb.models.waterData.WaterDAO;
+
+public interface WaterService{
+    
+    List<WaterDAO> getAllWaterData();
+    WaterDAO addWaterData1(final WaterDAO water);
+    WaterDAO addWaterData2(final WaterDAO water);
+    WaterDAO getWaterDataByUserName_month(String userName, String month);
+   
+}
