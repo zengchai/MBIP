2423823bf1a9dd1bc0e62943599ce2f65d5fc296
lazyyam <yyzhan020623@gmail.com>
2024-02-02 02:33:23 +0800

    fefd

--- tutorial/user.sql
+++ tutorial/user.sql
@@ -10,5 +10,7 @@ CREATE TABLE users (
     Category VARCHAR(20),
     Poscode VARCHAR(20),
     Address VARCHAR(100),
-    Level INT
+    Level INT,
+    Winner VARCHAR(40),
+    CarbonReduction INT
 );
