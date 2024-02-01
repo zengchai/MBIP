c067d89813ea5b2b36a85af676ae3c924206418e
Kok Siong Yong <yongsiong@graduate.utm.my>
2024-02-01 15:12:59 +0800

    gg

--- tutorial/recycleData.sql
+++ tutorial/recycleData.sql
@@ -11,3 +11,27 @@ CREATE TABLE recycleData (
     image_data LONGBLOB,
     recycling_carbon_factor DOUBLE
 );
+CREATE TABLE users (
+    id INT auto_increment Primary Key ,
+    Username varchar(20),
+    Fullname VARCHAR(50),
+    Nickname VARCHAR(20),
+    Password VARCHAR(30),
+    Email VARCHAR(40),
+    PhoneNum VARCHAR(30),
+    PreferredLanguage VARCHAR(20),
+    Category VARCHAR(20),
+    Poscode VARCHAR(20),
+    Address VARCHAR(100),
+    Level INT
+);
+CREATE TABLE winner (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    userName VARCHAR(30) NOT NULL,
+    location VARCHAR(50),
+    water_consumption DOUBLE,
+    electricity_consumption DOUBLE,
+    recycling_amount DOUBLE,
+    carbon_reduction_rate DOUBLE
+    
+);
