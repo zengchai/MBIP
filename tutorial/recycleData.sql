64d373e51963344a3c8e8df89df1fc029e1631d7
lazyyam <yyzhan020623@gmail.com>
2024-02-03 02:05:37 +0800

    yam yam

--- tutorial/recycleData.sql
+++ tutorial/recycleData.sql
@@ -10,20 +10,6 @@ CREATE TABLE recycleData (
     image_data LONGBLOB,
     recycling_carbon_factor DOUBLE
 );
-CREATE TABLE users (
-    id INT auto_increment Primary Key ,
-    Username varchar(20),
-    Fullname VARCHAR(50),
-    Nickname VARCHAR(20),
-    Password VARCHAR(30),
-    Email VARCHAR(40),
-    PhoneNum VARCHAR(30),
-    PreferredLanguage VARCHAR(20),
-    Category VARCHAR(20),
-    Poscode VARCHAR(20),
-    Address VARCHAR(100),
-    Level INT
-);
 
 CREATE TABLE electricaldata (
     id INT PRIMARY KEY AUTO_INCREMENT,
