6c0d52d33ca116f3069da219a16199981d04c4f2
lazyyam <yyzhan020623@gmail.com>
2024-02-06 03:44:03 +0800

    yam gonna push

--- src/main/java/my/utm/ip/zebb/services/winner/WinnerService.java
+++ src/main/java/my/utm/ip/zebb/services/winner/WinnerService.java
@@ -8,11 +8,7 @@ public interface WinnerService{
     List<Winner> getWinnersByUserAndMonth();
     Winner updateWinner(final Winner winner);
     int getWinnerCountForMonth(String month);
-<<<<<<< HEAD
-    List<WinnerDAO> getWinners();
     boolean deleteWinnerByUsername(String userName);
-=======
     List<Winner> getWinners();
->>>>>>> b11b206c0465b497d9f2c53f2c108826a8d8912c
 
 }
